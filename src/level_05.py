68a613a92fbfbd7c9e72573e7868159b50893813
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2020-10-29 23:56:39 +0300

    feat: Solution for level 05

---
+++ src/level_05.py
@@ -0,0 +1,21 @@
+import os
+import pickle
+from typing import Tuple, List
+
+
+def solution(filename: str):
+    data = unpickling(filename)
+    for row in data:
+        line = "".join([ch * cnt for ch, cnt in row])
+        print(line)
+
+
+def unpickling(filename: str) -> List[List[Tuple[str, int]]]:
+    dir_path = os.path.dirname(__file__)
+    file_path = os.path.join(dir_path, f"../data/{filename}")
+    with open(file_path, "rb") as f:
+        decoded = pickle.load(f, encoding='utf-8')
+        return decoded
+
+
+solution("banner.p")
