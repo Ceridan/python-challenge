bf69078532a17056b49ee03a0c7ab0b4457d0f89
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2020-11-13 01:23:36 +0300

    feat: Solution for level 14

---
+++ src/level_14.py
@@ -0,0 +1,33 @@
+from PIL import Image
+
+
+def solution(picture_filename: str):
+    with Image.open(f"data/{picture_filename}") as im:
+        pixels = im.load()
+        width, height = 100, 100
+        new_img = Image.new("RGB", (width, height))
+
+        index, level = 0, 0
+        for i in range(100, 1, -2):
+            for j in range(i):
+                new_img.putpixel((level + j, level), pixels[index, 0])
+                index += 1
+
+            for j in range(i - 1):
+                new_img.putpixel((width - level - 1, level + j + 1), pixels[index, 0])
+                index += 1
+
+            for j in range(i - 1):
+                new_img.putpixel((width - level - 1 - j, height - level - 1), pixels[index, 0])
+                index += 1
+
+            for j in range(i - 2):
+                new_img.putpixel((level, height - level - 1 - j), pixels[index, 0])
+                index += 1
+
+            level += 1
+
+        new_img.show()
+
+
+solution("wire.png")
